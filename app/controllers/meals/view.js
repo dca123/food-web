590d431cb5381b99efd1ab9b328ee194c94f24d2
Devinda Senanayaka <dsenanayaka1@gmail.com>
2019-01-07 23:37:51 +0530

    added delete for meal

--- app/controllers/meals/view.js
+++ app/controllers/meals/view.js
@@ -2,5 +2,15 @@ import newController from './new'
 //Extending the new controller
 export default newController.extend({
   queryParams: ['isEditing'],
-  isEditing: false
+  isEditing: false,
+  actions: {
+    deleteMeal(){
+      this.set('isDeleting', true);
+      let meal = this.get('model');
+      meal.destroyRecord().then((data) => {
+        this.set('isDeleting', false);
+        this.transitionToRoute('meals.index')
+      })
+    }
+  }
 });
